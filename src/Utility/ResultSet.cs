21d295e1d12eccf15ed0cfc17c1a7e3edc98ac36
Hawk_200014 <damienbecker20@gmail.com>
2025-02-01 23:26:59 +0100

    Added ResultSet Class to have better result handling (no exceptions) Added Singleton class to implement the singleton pattern

---
+++ src/Utility/ResultSet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FallenTally.Utility
+{
+    public class ResultSet<T>
+    {
+        #region Result Enum
+        public enum Result
+        {
+            SUCCESS = 0,
+            FAILURE = 1
+        }
+        #endregion
+
+        private Result _result;
+        private string _message;
+        private T _data;
+                     
+        public ResultSet(Result result, T data, string message = "")
+        {
+            _result = result;
+            if (_result == Result.SUCCESS && message == "")
+            {
+                _message = "Success";
+            }
+            else
+            {
+                _message = message;
+            }
+            _data = data;
+        }
+
+        public T GetData()
+        {
+            return _data;
+        }
+
+        public string GetMessage()
+        {
+            return _message;
+        }
+
+        public Result GetResult()
+        {
+            return _result;
+        }
+
+    }
+}
---
+++ src/Utility/Singleton.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FallenTally.Utility
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+
+        private Dictionary<string, object> keyValuePairs = new Dictionary<string, object>();
+
+        public Singleton()
+        {
+            instance = this;
+        }
+
+        #region Getters
+        public static Singleton GetInstance()
+        {
+            if (instance is null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        public string[] GetKeys()
+        {
+            return keyValuePairs.Keys.ToArray();
+        }
+
+        public object GetValue(string key)
+        {
+            if (keyValuePairs.ContainsKey(key))
+            {
+                return keyValuePairs[key];
+            }
+            return null;
+        }
+
+        #endregion
+        #region Setters
+
+
+
+        #endregion
+
+        public void Add(string key, object value)
+        {
+            if (keyValuePairs.ContainsKey(key))
+            {
+                keyValuePairs[key] = value;
+            }
+            else
+            {
+                keyValuePairs.Add(key, value);
+            }
+        }
+    }
+}
